cb5d71058a16121ef680491b2fbbf230e36bd359
Tech Mobi <Behzodbek12>
2024-12-26 13:52:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react'
+import Navbar from './compak/Navbar'
+import "./App.css"
+  const App = () => {
+  return (
+    <> 
+<Navbar/>
+</>
+  )
+}
+
+export default App
---
+++ src/compak/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <div>
+          <div className='harif'>
+        <button className='f'>F</button>
+        <div className='h'>
+            <h1>o'odi</h1>
+        </div>
+        <div className='alo'>
+            <a href="">Home</a>
+            <a href="">Menu</a>
+            <a href="">Services</a>
+            <a href="">Offers</a>
+        </div>
+        <div className='yozu'>
+            <h1> Dive into Delights Of Delectable Food</h1>
+            <h2>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</h2>
+        </div>
+        <div className='bos'>
+          <button>Order Now</button>
+          <button className='watch'>Watch Video</button>
+        </div>
+        <div className='rasm'>
+        </div>
+        <div className='yoz'>
+          <h1>Customer Favorites</h1>
+          <h2>Popular Catagories</h2>
+        </div>
+        <div className='ras'>
+        </div>
+        <div className='menu'>
+          <h1>Special Dishes</h1>
+          <h2>Standout Dishes From Our Menu</h2>
+          <div className='but'>
+          <button className='butt'> +  </button>
+          <button className='butto'> - </button>
+          </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+export default Navbar
